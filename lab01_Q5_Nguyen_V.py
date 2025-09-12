6ba60bd3989f71f1ebb7bb7af71fb398d13cf9a5
vincentn125 <vincentnguyen1206@gmail.com>
2025-09-11 20:44:41 -0700

    Almost final draft of lab1_Q6

---
+++ lab01_Q5_Nguyen_V.py
@@ -0,0 +1,11 @@
+# Vincent Nguyen
+# 1320229
+
+# Lab 1 Question 5
+# This program aks the user to enter the projected amount of total sales
+# then displays the profit that will be
+# made from that amount.
+
+# User Input
+
+totalSales = input("Enter the projected total sales: ")
