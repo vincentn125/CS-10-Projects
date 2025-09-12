18ebd3e7aeec2873f1f1ffea01aadc07ef2ff81e
vincentn125 <vincentnguyen1206@gmail.com>
2025-09-11 21:06:26 -0700

    first draft of lab1_Q7

--- lab01_Q7_Nguyen_V.py
+++ lab01_Q7_Nguyen_V.py
@@ -2,3 +2,11 @@
 # 1320229
 
 # Lab 1 Question 7
+# This program prompts the user to enter a four-digit integer 
+# and displays the number in reverse order.
+
+userNumber = int(input("Enter an integer: "))
+
+firstNumber = userNumber // 1000
+
+firstNumberRemainder = userNumber % 1000
