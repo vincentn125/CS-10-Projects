9cdf6cb80c06202f02c9a7d9f7c3dbba3a84ad0a
vincentn125 <vincentnguyen1206@gmail.com>
2025-09-15 19:49:21 -0700

    final draft of lab 1 Q8

--- lab01_Q8_Nguyen_V.py
+++ lab01_Q8_Nguyen_V.py
@@ -5,10 +5,22 @@
 # Program calculate the gravitational force (F)
 # and acceleration due to gravity (g) caused by gravitational force exerted by the Earth
 
+# constants
+G = 6.67300 * (10**-11)
+earthRadius = 6378 * (10.0**3)
+m1 = 5.9742 * (10**24)
+
 # input
+userMassKG = float(input("Enter a mass in kg: "))
 
 # calculations/processing
+gResult = ((((G * m1 * userMassKG))/(earthRadius ** 2)) / (userMassKG))
 
 # output
+print("The resulting value of g is", format(gResult, ',.4f'), "which is close to the earth's gravitational force")
 
-# Test case 1
+## Test case 1
+## Enter a mass in kg: 30
+## The resulting value of g is 9.8001 which is close to the earth's gravitational force
+##
+
