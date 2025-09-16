6bf3750f83e6921e071d7827cb4cf57ed345149a
vincentn125 <vincentnguyen1206@gmail.com>
2025-09-15 20:56:05 -0700

    Editing first stock info

--- HW1_PS1_Nguyen_V.py
+++ HW1_PS1_Nguyen_V.py
@@ -1,1 +1,31 @@
-stockOneInput = "Enter"
+# Student Name: Vincent Nguyen
+# ID: 1320229
+# Homework 1, Program Set 1
+
+# Program tracks and reports 3 stocks on its costs and profit or loss
+
+# input for stock 1
+print("*********************************")
+print("* Enter Information for Stock 1 *")
+print("*********************************")
+stock1Name = input("Enter Stock Name in Uppercase: ")
+stock1NumOfShares = float(input("Enter Number of Shares  :  "))
+stock1BuyPrice = float(input("Enter Purchase Price  :  "))
+stock1SellPrice = float(input("Enter Selling Price  :  "))
+stock1Comm = float(input("Enter Commission (in percent) :  "))
+
+# calculations on stock 1
+stock1AmountPaid = (stock1NumOfShares * stock1BuyPrice)
+stock1CommFloat = (stock1Comm / 100)
+stock1CommBuy = (stock1AmountPaid * stock1CommFloat)
+stock1SoldFor = (stock1NumOfShares * stock1SellPrice)
+stock1CommSell = (stock1SoldFor * stock1CommFloat)
+stock1ProfOrLoss = (stock1SoldFor - stock1CommSell) - (stock1AmountPaid + stock1CommBuy)
+
+# output on stock 1
+print("")
+print("")
+print("------------------------------------------------------------")
+print(stock1Name)
+print("")
+#print("Amount paid for the stock:          $", "{:13s}".format(stock1AmountPaid, ',.2f'))
